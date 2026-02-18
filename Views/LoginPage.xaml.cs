317b82aff915bd244f54de3d92c23389336ef2b4
Artem <q>
2026-02-18 23:47:00 +0200

    fix LoginPage build errors

---
+++ Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace Courses.Views
+{
+    /// <summary>
+    /// Логіка взаємодії для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
